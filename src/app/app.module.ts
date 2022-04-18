01be3bb457809b31464635f79a8f7128941f5cf5
rennan-sbarros <rennan_sbarros@hotmail.com>
2022-04-18 11:11:24 -0300

    fix: correção do módulo container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,23 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContainerComponent } from './container/container.component';
 import { ContainerModule } from './container/container.module';
 import { HomeModule } from './pages/home/home.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContainerComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeModule,
-    ContainerModule,
     BrowserAnimationsModule,
+    ContainerModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/container/container.module.ts
+++ src/app/container/container.module.ts
@@ -1,21 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
-import { ContainerRoutingModule } from './container-routing.module';
-import { FooterComponent } from './footer/footer.component';
+import { ContainerComponent } from './container.component';
 import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    ContainerComponent
   ],
   imports: [
     CommonModule,
-    ContainerRoutingModule,
   ],
-  exports: [HeaderComponent, FooterComponent],
+  exports: [
+    ContainerComponent
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ContainerModule { }
