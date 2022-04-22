5d0312bbc5a304cbaece0732fcadba4dc3841b1a
rennan-sbarros <rennan_sbarros@hotmail.com>
2022-04-22 11:41:11 -0300

    feat: guarda de rota para usuario logado não acessar tela de login novamente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AutenticacaoGuard } from './autenticacao/autenticacao.guard';
+import { LoginGuard } from './autenticacao/login.guard';
 
 const routes: Routes = [
   {
@@ -10,12 +12,14 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module')
-    .then((m) => m.HomeModule)
+      .then((m) => m.HomeModule),
+    canLoad: [LoginGuard]
   },
   {
     path: 'animais',
     loadChildren: () => import('./animais/animais.module')
-    .then((m) => m.AnimaisModule)
+      .then((m) => m.AnimaisModule),
+    canLoad: [AutenticacaoGuard],
   }
 ];
 
